bfdb055e0ba6494597a80db9434933b236ed3aef
Hoooao <hao021014@163.com>
2022-11-08 23:44:59 -0500

    mile2 finished

--- server/express.mjs
+++ server/express.mjs
@@ -2,16 +2,15 @@ import path from 'path';
 import url from 'url';
 import express from 'express';
 import SearchRouter from './routes/search.mjs'
-import fs from 'fs/promises'
 
 const __dir = path.dirname(url.fileURLToPath(import.meta.url));
 const app = express();
-app.use(express.urlencoded({extended: false}));
+app.use(express.json({extended: false}));
 app.use(express.static(path.join(__dir, '../build')));
 
 app.use('/api',SearchRouter);
 
-app.get('*', (req, res,next) => {
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dir, '../build', 'index.html'));
 });
 
--- server/routes/search.mjs
+++ server/routes/search.mjs
@@ -3,16 +3,34 @@ import pool from '../db.mjs';
 
 const router = express.Router();
 
-router.get('/search',async(req,res)=>{
+router.get('/search', async (req, res) => {
     const content = req.query.content;
     //Simly for the convinence of milestone 2, will be changed to MVC form
     const queryRes = await pool.query(`select * from user 
-        where user.User = ?`,[content]);
-        console.log(queryRes[0][0])
-    if(queryRes[0].length==0){
-        res.send({ok:0,results:queryRes[0]});
-    }else res.send({ok:1,results:queryRes[0][0].Host});
+        where user.User = ?`, [content]);
+    console.log(queryRes[0][0])
+    if (queryRes[0].length == 0) {
+        res.send({ ok: 0, results: queryRes[0] });
+    } else res.send({ ok: 1, results: queryRes[0][0].Host });
 })
 
+router.get('/show',async () => {
+    const queryRes = await pool.query(`select * from posts 
+    `);
+    console.log(queryRes[0])
+    if (queryRes[0].length == 0) {
+        res.send({ ok: 0, results: queryRes[0] });
+    } else res.send({ ok: 1, results: queryRes[0] });
+})
+
+// This route is simply for Mile2:
+
+router.post('/submit', async (req, res) => {
+    const body = req.body;
+    console.log(req.body)
+    const queryRes = await pool.query(`insert into posts (title,content) 
+        values (?)`, [body['title'], body['content']]);
+    res.send({ ok: 0 });
+})
 
 export default router;
